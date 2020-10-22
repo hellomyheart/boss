701bec57503b0c3d63f0dca28ccd45121309f7c6
xz <2684127868@qq.com>
2020-10-23 07:53:49 +0800

    发布招聘信息V1.1

--- src/main/java/com/example/boss/mapper/RecruitMapper.java
+++ src/main/java/com/example/boss/mapper/RecruitMapper.java
@@ -1,15 +1,13 @@
 package com.example.boss.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.boss.dto.RecruitDto2;
 import com.example.boss.entity.Recruit;
-import org.apache.ibatis.annotations.Insert;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecruitMapper extends BaseMapper<Recruit> {
 
-    @Insert("insert into recruit(p_id,num,address,createtime,updatetime) values(#{p_id},#{num},#{address},#{createtime},#{updatetime})")
-    int insertDto(RecruitDto2 recruit);
+//    @Insert("insert into recruit(p_id,num,address,createtime,updatetime) values(#{p_id},#{num},#{address},#{createtime},#{updatetime})")
+//    int insertDto(RecruitDto2 recruit);
 
 }
--- src/main/java/com/example/boss/service/impl/RecruitServiceImpl.java
+++ src/main/java/com/example/boss/service/impl/RecruitServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.boss.service.impl;
 
 import com.example.boss.dto.RecruitDto;
-import com.example.boss.dto.RecruitDto2;
 import com.example.boss.entity.Recruit;
 import com.example.boss.mapper.RecruitMapper;
 import com.example.boss.service.RecruitService;
@@ -18,8 +17,9 @@ public class RecruitServiceImpl implements RecruitService {
 
     @Override
     public ResponseResult sendResume(RecruitDto recruit) {
-        RecruitDto2 recruit1 = new RecruitDto2(recruit.getP_id(),recruit.getNum(),recruit.getAddress(),new Date(),new Date());
-        int insert = recruitMapper.insertDto(recruit1);
+        Recruit recruit1 = new Recruit(recruit.getP_id(),recruit.getNum(),recruit.getAddress(),new Date(),new Date());
+        recruit1.setId(0);
+        int insert = recruitMapper.insert(recruit1);
         if(insert != 0){
             return ResponseResult.ok();
         }
