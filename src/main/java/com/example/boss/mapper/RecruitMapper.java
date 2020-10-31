e924ddfd6192e1eaa6ca29066feb39e8a5e0509f
stephen shen <723809534@qq.com>
2020-10-31 14:35:18 +0800

    v2.1 招聘信息表及其日志表更新

--- src/main/java/com/example/boss/mapper/RecruitMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.boss.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.boss.entity.Recruit;
-
-public interface RecruitMapper extends BaseMapper<Recruit> {
-}
--- src/main/java/com/example/boss/service/RecruitService.java
+++
@@ -1,8 +0,0 @@
-package com.example.boss.service;
-
-import com.example.boss.entity.Recruit;
-import com.baomidou.mybatisplus.extension.service.IService;
-public interface RecruitService extends IService<Recruit>{
-
-
-}
